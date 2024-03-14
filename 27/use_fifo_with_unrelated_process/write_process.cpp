504774f83c300f39f4ec068346c240c3fbd85fb4
Ragab2010 <ragabmx2002@gmail.com>
2024-03-14 22:16:51 +0200

    using named pipe (FIFO) to communicate between  un related process

---
+++ 27/use_fifo_with_unrelated_process/write_process.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string>
+
+
+using namespace std;
+
+int main(){
+    //  int mknod(const char *pathname, mode_t mode, dev_t dev);
+    mknod("myfifo" ,S_IFIFO |0666 , 0 );
+    cout<<"writer process ready to sends msgs:"<<endl;
+    auto fd = open("myfifo" , O_WRONLY );
+    while(cin){
+        string buff;
+        getline(cin , buff);
+        write(fd , buff.c_str() , buff.size()+1);
+    }
+    close(fd);
+    return 0;
+}
