cc72512d85095dd5c5d19e1f26423ec90ee4651d
Ragab2010 <ragabmx2002@gmail.com>
2024-03-14 16:52:49 +0200

    perform this command line :cat f1.txt | wc

---
+++ 26/perform_cat_f1.txt_|_wc.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include <unistd.h>
+#include <fcntl.h>              /* Obtain O_* constant definitions */
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/*
+perform this command line :cat f1.txt | wc
+*/
+using namespace std;
+
+int main(){
+    int fd[2];
+
+    //create pipe
+    auto rs = pipe(fd);
+
+    if(rs == -1){
+        cerr<<"pipe faild"<<endl;
+        return 1;
+    }
+    auto pid = fork();
+    if(pid == 0){
+        //child process
+        if(dup2(fd[0] , STDIN_FILENO) == -1){
+            perror("dup2 STDOUT faild");
+            return 1;
+        }
+        close(fd[1]);
+        execlp("wc" , "mywc" , nullptr);
+        
+    }else{
+        //parent  process
+        if(dup2(fd[1] , STDOUT_FILENO) == -1){
+            perror("dup2 STDOUT faild");
+            return 1;
+        }
+        close(fd[0]);
+        execlp("cat" , "mycat" , "f1.txt" , nullptr);
+    }
+    return 0;
+}
