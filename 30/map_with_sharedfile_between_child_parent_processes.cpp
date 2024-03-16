0e3954eea709b1a3f354f3506362a7145b227aac
Ragab2010 <ragabmx2002@gmail.com>
2024-03-16 17:19:44 +0200

    map shared file between child and parent process  by using mmap and fork

---
+++ 30/map_with_sharedfile_between_child_parent_processes.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+
+
+
+
+using namespace std;
+
+int main(){
+    //open f1.txt
+    auto fd = open("./f1.txt" , O_RDONLY);
+    if(fd == -1){
+        cerr<<"there is an problem  with opening txt file "<<endl;
+        return 1; 
+    }
+
+    //calculate the size of the file into bytes
+    //off_t lseek(int fd, off_t offset, int whence);
+    /*
+    The first parameter specifies the file descriptor
+    The second parameter is offset
+    The third parameter specifies the from where we start the offset
+    return:the number of the byte (character) it seek
+    */
+    //int file_size = lseek(fd , 0 , SEEK_END);
+    struct stat statbuf;
+    fstat(fd , &statbuf);
+    int file_size = statbuf.st_size;
+
+    //memory map the file content
+    /*
+    The first parameter (nullptr) To choose the  kernel  select  the  address of memory map at the address space of this process 
+    
+    */
+    char * file_ptr =(char *) mmap(nullptr , file_size , PROT_READ , MAP_SHARED , fd , 0);
+    if(file_ptr == nullptr){
+        cerr<<"mmap creation failed"<<endl;
+        return 1;
+    }
+    auto cpid = fork();
+    if(cpid == 0){
+        cout<<"Child access memory mapped file:\n"<<file_ptr<<endl;
+        munmap(file_ptr, file_size);
+        close(fd);
+        exit(0);
+    }else{
+        wait(nullptr);
+        cout<<"Parent access memory mapped file:\n"<<file_ptr<<endl;
+        munmap(file_ptr, file_size);
+        close(fd);
+        exit(0);
+    }
+    return 0;
+}
