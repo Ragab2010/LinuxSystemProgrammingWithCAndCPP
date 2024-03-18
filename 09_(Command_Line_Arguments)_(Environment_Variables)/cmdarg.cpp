3fa8f034f7637d5e5b1788f35f42f8f0de4e6b66
Ragab2010 <ragabmx2002@gmail.com>
2024-03-19 00:14:11 +0200

    Command Line Arguments & Environment Variables

--- 09_(Command_Line_Arguments)_(Environment_Variables)/cmdarg.cpp
+++ 09_(Command_Line_Arguments)_(Environment_Variables)/cmdarg.cpp
@@ -8,7 +8,7 @@ int main(int argc , char **argv){
     if(nameOfProgram == "./hello"s){
         cout<<"I'm called by the name hello\n";
     }else {
-        cout<<"I'm called by the name byte\n";
+        cout<<"I'm called by the name cmdarg\n";
     }
     return 0;
 }
--- 09_(Command_Line_Arguments)_(Environment_Variables)/print_environmentVariables.cpp
+++ 09_(Command_Line_Arguments)_(Environment_Variables)/print_environmentVariables.cpp
@@ -5,7 +5,7 @@ using namespace std;
 extern char **environ; 
 
 int main(void){
-    for(int i = 0 ; environ[0] !=nullptr ; i++){
+    for(int i = 0 ; environ[i] !=nullptr ; i++){
         cout<<"environ["<<i<<"]="<<environ[i]<<'\n';
     }
     return 0;
