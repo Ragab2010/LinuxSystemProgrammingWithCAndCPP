5450f8e1d1ac24a316d78f4ae2f923530224545e
Ragab2010 <ragabmx2002@gmail.com>
2024-03-19 19:22:13 +0200

    usage of a pipe withing a single process

--- 26_(pipe)_(unidirectional_and_bidirectional)/simple_unidirectional_pipe.cpp
+++ 26_(pipe)_(unidirectional_and_bidirectional)/simple_unidirectional_pipe.cpp
@@ -1,6 +1,10 @@
+/*
+*  A hello world program on pipe
+*  shows the usage of a pipe withing a single process
+*/
 #include<iostream>
 #include <unistd.h>
-#include <fcntl.h>              /* Obtain O_* constant definitions */
+#include <fcntl.h>
 #include <unistd.h>
 
 
@@ -15,17 +19,19 @@ int main(){
         return 1;
     }
     string msgString{"Hello , world! (Using pipe)"};
+    cout<<"here we going to write for pipe string :"<<msgString<<endl;
     ssize_t write(int fd, const void *buf, size_t count);
     //write to pipe
     auto chWrite = write(fd[1] , msgString.c_str() , msgString.size()+1);
 
+    cout<<"here we going to read for pipe:"<<endl;
     //read from pipe
     string recievedMsg(50 , 0);
     auto chRead = read(fd[0] ,&recievedMsg[0] , recievedMsg.size() );
     //display the data on stdout
     recievedMsg.shrink_to_fit();
     cout<<recievedMsg<<endl;
-    write(1 , recievedMsg.c_str() , recievedMsg.size());
+    //write(1 , recievedMsg.c_str() , recievedMsg.size());
     while(1);
     return 0;
 }
